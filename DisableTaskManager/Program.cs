615d4c205ae650581849372f8b2dd28007407429
Alp Doğan <alp48832@gmail.com>
2023-01-14 17:31:14 +0300

    Adding Files

---
+++ DisableTaskManager/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisableTaskManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if ((string)Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableTaskMgr", 0) == "1")
+            {
+                Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableTaskMgr", 0);
+            } else
+            {
+                Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableTaskMgr", 1);
+            }        
+        }
+    }
+}
